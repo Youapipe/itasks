f6becb80d8758eb3b6bb37cd52f8e34fb6dfd783
Youapipe <vags211@gmail.com>
2023-03-16 21:24:00 +0300

    Added Robot

---
+++ src/main/java/ru/shockshaw/Robot.java
@@ -0,0 +1,36 @@
+package ru.shockshaw;
+
+import javax.print.attribute.standard.DialogOwner;
+
+public class Robot {
+    private int x;
+    private int y;
+    Direction direction;
+
+    public Robot(int x, int y, Direction direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+    public void setDirection(Direction toSet) {direction = toSet;}
+    public Direction getDirection() {return direction;}
+    public void move() {
+        switch (direction) {
+            case UP:
+                y += 1;
+                break;
+            case DOWN:
+                y -= 1;
+                break;
+            case RIGHT:
+                x += 1;
+                break;
+            case LEFT:
+                x -= 1;
+                break;
+        }
+    }
+    public int getX() {return x;}
+    public int getY() {return y;}
+}
